ac7a277f0fb00285f6c1d142bd3158e00acef49e
dymanoid <9433345+dymanoid@users.noreply.github.com>
2018-08-02 20:06:34 +0200

    Create solution and project

---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="AssemblyInfo.cs" company="dymanoid">
+// Copyright (c) dymanoid. All rights reserved.
+// </copyright>
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SkyTools")]
+[assembly: AssemblyDescription("Common tools for Cities: Skylines game mods")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SkyTools")]
+[assembly: AssemblyCopyright("Copyright © 2018, dymanoid")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
