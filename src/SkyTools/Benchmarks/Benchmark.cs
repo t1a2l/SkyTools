45f024b2089275f022450df445185cb2ef4a1803
dymanoid <9433345+dymanoid@users.noreply.github.com>
2018-08-04 13:50:23 +0200

    Implement the method benchmark tools

---
+++ src/SkyTools/Benchmarks/Benchmark.cs
@@ -0,0 +1,134 @@
+﻿// <copyright file="Benchmark.cs" company="dymanoid">
+// Copyright (c) dymanoid. All rights reserved.
+// </copyright>
+
+namespace SkyTools.Benchmarks
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using SkyTools.Patching;
+    using SkyTools.Tools;
+
+    /// <summary>
+    /// A method execution benchmark. Activate it after the game level is loaded.
+    /// Call the <see cref="MakeSnapshot"/> method periodically to gather the performance data snapshots.
+    /// The calling frequency should correlate with the data window size you specify when creating an instance.
+    /// </summary>
+    public sealed class Benchmark
+    {
+        private const string HarmonyId = "com.cities_skylines.dymanoid.skytools.benchmarks";
+        private readonly List<IPatch> patches = new List<IPatch>();
+        private MethodPatcher patcher;
+        private bool isRunning;
+
+        private Benchmark()
+        {
+        }
+
+        /// <summary>Creates a new instance of the benchmark.</summary>
+        /// <param name="windowSize">The Size of the data window to use for data collection.</param>
+        /// <returns>A new instance of the <see cref="Benchmark"/> class.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when the window size is less than 16.</exception>
+        public static Benchmark Create(int windowSize)
+        {
+            if (windowSize < 16)
+            {
+                throw new ArgumentOutOfRangeException(nameof(windowSize), windowSize, "The window size must be greater than or equal to 16");
+            }
+
+            BenchmarkPatch.DataCollector = new DataCollector(windowSize);
+            return new Benchmark();
+        }
+
+        /// <summary>Adds the specified method to this benchmark.</summary>
+        /// <param name="type">The type that contains the method.</param>
+        /// <param name="methodName">Name of the method to benchmark.</param>
+        /// <param name="methodParameters">The method parameters.</param>
+        /// <returns><c>true</c> if the method was added to the benchmark; otherwise, <c>false</c>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="type"/> is null.</exception>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="methodName"/>> is null or an empty string.</exception>
+        public bool BenchmarkMethod(Type type, string methodName, params Type[] methodParameters)
+        {
+            if (type == null)
+            {
+                throw new ArgumentNullException(nameof(type));
+            }
+
+            if (string.IsNullOrEmpty(methodName))
+            {
+                throw new ArgumentException("The method name cannot be null or an empty string", nameof(methodName));
+            }
+
+            try
+            {
+                var patch = new BenchmarkPatch(type, methodName, methodParameters);
+                patches.Add(patch);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"Cannot benchmark the specified method {type.FullName}.{methodName}: {ex}");
+                return false;
+            }
+        }
+
+        /// <summary>Starts the benchmarking by applying the corresponding method patches.</summary>
+        /// <returns><c>true</c> on success, <c>false</c> on failure.</returns>
+        public bool Start()
+        {
+            BenchmarkPatch.DataCollector.Clear();
+            patcher = new MethodPatcher(HarmonyId, patches);
+
+            try
+            {
+                patcher.Apply();
+                isRunning = true;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Failed to start benchmarking: " + ex);
+                return false;
+            }
+        }
+
+        /// <summary>Stops the benchmarking if it is running.</summary>
+        public void Stop()
+        {
+            if (!isRunning)
+            {
+                return;
+            }
+
+            patcher.Revert();
+            isRunning = false;
+        }
+
+        /// <summary>Makes a snapshot of currently recorded performance data.
+        /// This method should be called periodically while the benchmarking is running.</summary>
+        public void MakeSnapshot()
+        {
+            BenchmarkPatch.DataCollector.MakeSnapshot();
+        }
+
+        /// <summary>Dumps the currently available performance data to the game's log file.</summary>
+        public void Dump()
+        {
+            if (patches.Count == 0)
+            {
+                return;
+            }
+
+            var methods = patches.Cast<BenchmarkPatch>().Select(p => p.Method).ToList();
+
+            string header =
+                "------------------------------------------------------------------" + Environment.NewLine +
+                "-----                       SkyTools Benchmark             -------" + Environment.NewLine +
+                string.Join(";", methods.Select(m => m.ToFullString()).ToArray());
+
+            string data = BenchmarkPatch.DataCollector.Dump(methods);
+            Log.Info(header + Environment.NewLine + data);
+        }
+    }
+}
---
+++ src/SkyTools/Benchmarks/DataCollector.cs
@@ -0,0 +1,188 @@
+﻿// <copyright file="DataCollector.cs" company="dymanoid">
+// Copyright (c) dymanoid. All rights reserved.
+// </copyright>
+
+namespace SkyTools.Benchmarks
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+
+    /// <summary>
+    /// An utility class that collects the performance data of the methods.
+    /// </summary>
+    internal sealed class DataCollector
+    {
+        private readonly object syncObject = new object();
+        private readonly Dictionary<MethodInfo, MethodPerformance> counters = new Dictionary<MethodInfo, MethodPerformance>();
+        private readonly List<Snapshot> snapshots = new List<Snapshot>();
+
+        private readonly int averagingWindow;
+
+        /// <summary>Initializes a new instance of the <see cref="DataCollector"/> class.</summary>
+        /// <param name="averagingWindow">The averaging window size for statistics.</param>
+        public DataCollector(int averagingWindow)
+        {
+            this.averagingWindow = averagingWindow;
+        }
+
+        /// <summary>Records a sample for the specified <paramref name="method"/>.</summary>
+        /// <param name="method">The method to record the performance sample for.</param>
+        /// <param name="elapsed">The method execution time in ticks.</param>
+        public void RecordSample(MethodInfo method, long elapsed)
+        {
+            lock (syncObject)
+            {
+                if (!counters.TryGetValue(method, out MethodPerformance stats))
+                {
+                    stats = new MethodPerformance(averagingWindow);
+                    counters.Add(method, stats);
+                }
+
+                stats.AddSample(elapsed);
+            }
+        }
+
+        /// <summary>Clears all data this instance currently holds.</summary>
+        public void Clear()
+        {
+            lock (syncObject)
+            {
+                counters.Clear();
+            }
+
+            snapshots.Clear();
+        }
+
+        /// <summary>Makes a snapshot of the data this instance currently holds
+        /// and adds this snapshot in the snapshots history.</summary>
+        public void MakeSnapshot()
+        {
+            var snapshotData = new Dictionary<MethodInfo, long[]>();
+            lock (syncObject)
+            {
+                foreach (KeyValuePair<MethodInfo, MethodPerformance> item in counters)
+                {
+                    snapshotData[item.Key] = item.Value.GetSnapshot();
+                }
+            }
+
+            var snapshot = new Snapshot();
+            foreach (KeyValuePair<MethodInfo, long[]> item in snapshotData)
+            {
+                snapshot.Data[item.Key] = MethodSnapshot.Calculate(item.Value);
+            }
+
+            snapshots.Add(snapshot);
+        }
+
+        /// <summary>Dumps all snapshots contained in the snapshots history to a semicolon-separated string .</summary>
+        /// <param name="methods">The methods to dump the information for.</param>
+        /// <returns>A semicolon-separated string containing the recorded performance data.</returns>
+        public string Dump(IEnumerable<MethodInfo> methods)
+        {
+            var sb = new StringBuilder(1024);
+            foreach (Snapshot snapshot in snapshots)
+            {
+                foreach (MethodInfo method in methods)
+                {
+                    if (snapshot.Data.TryGetValue(method, out var data))
+                    {
+                        sb.Append(data.SamplesCount);
+                        sb.Append(';');
+                        sb.Append(data.Average);
+                        sb.Append(';');
+                        sb.Append(data.Median);
+                        sb.Append(';');
+                    }
+                    else
+                    {
+                        sb.Append(";;;");
+                    }
+                }
+
+                sb.AppendLine();
+            }
+
+            return sb.ToString();
+        }
+
+        private readonly struct MethodSnapshot
+        {
+            private MethodSnapshot(int samplesCount, long average, long median)
+            {
+                SamplesCount = samplesCount;
+                Average = average;
+                Median = median;
+            }
+
+            public int SamplesCount { get; }
+
+            public long Average { get; }
+
+            public long Median { get; }
+
+            public static MethodSnapshot Calculate(long[] data)
+            {
+                if (data.Length == 0)
+                {
+                    return default;
+                }
+
+                Array.Sort(data);
+                long median;
+                if (data.Length % 2 == 0)
+                {
+                    int middle = data.Length / 2;
+                    median = (data[middle - 1] + data[middle]) / 2;
+                }
+                else
+                {
+                    median = data[data.Length / 2];
+                }
+
+                return new MethodSnapshot(data.Length, (long)data.Average(), median);
+            }
+        }
+
+        private sealed class Snapshot
+        {
+            public Dictionary<MethodInfo, MethodSnapshot> Data { get; }
+        }
+
+        private sealed class MethodPerformance
+        {
+            private readonly long[] samples;
+            private int current = -1;
+
+            public MethodPerformance(int averagingWindow)
+            {
+                samples = new long[averagingWindow];
+            }
+
+            public long[] GetSnapshot()
+            {
+                int count = current + 1;
+                long[] result = new long[count];
+                if (count > 0)
+                {
+                    Array.Copy(samples, result, count);
+                }
+
+                return result;
+            }
+
+            public void AddSample(long elapsed)
+            {
+                if (++current == samples.Length)
+                {
+                    current = 0;
+                }
+
+                samples[current] = elapsed;
+            }
+        }
+    }
+}
