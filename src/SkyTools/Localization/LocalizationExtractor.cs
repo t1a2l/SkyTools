c1ffe08a2d6176bb023aea5828674c3d8725109b
dymanoid <9433345+dymanoid@users.noreply.github.com>
2018-10-28 20:49:30 +0100

    Add a localization string extraction helper

---
+++ src/SkyTools/Localization/LocalizationExtractor.cs
@@ -0,0 +1,58 @@
+﻿// <copyright file="LocalizationExtractor.cs" company="dymanoid">
+// Copyright (c) dymanoid. All rights reserved.
+// </copyright>
+
+namespace SkyTools.Localization
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using ColossalFramework.Globalization;
+
+    /// <summary>
+    /// A helper class that extracts localized strings from the game (using the current game's language).
+    /// </summary>
+    public static class LocalizationExtractor
+    {
+        /// <summary>Extracts the localization strings and saves them to the specified file.</summary>
+        /// <param name="targetPath">The target file path to save the results to.</param>
+        /// <param name="attributeText">The optional string value that will be searched in the localization key attribute.</param>
+        /// <exception cref="ArgumentException">Thrown when the target path is null or an empty string.</exception>
+        public static void Extract(string targetPath, string attributeText)
+        {
+            if (string.IsNullOrEmpty(targetPath))
+            {
+                throw new ArgumentException("The target path cannot be null or an empty string", nameof(targetPath));
+            }
+
+            IEnumerable<FieldInfo> constants = GetConstants(typeof(LocaleID))
+                .Where(fi => fi.FieldType == typeof(string));
+
+            if (!string.IsNullOrEmpty(attributeText))
+            {
+                constants = constants
+                    .Where(fi => fi.GetCustomAttributes(typeof(LocaleAttribute), false)
+                        .Cast<LocaleAttribute>()
+                        .FirstOrDefault()?
+                        .file?
+                        .Contains(attributeText) ?? false);
+            }
+
+            using (var sw = new StreamWriter(targetPath))
+            {
+                foreach (FieldInfo constant in constants)
+                {
+                    string key = (string)constant.GetValue(null);
+                    sw.WriteLine($"<translation id=\"{key}\" value=\"{Locale.Get(key)}\"/>");
+                }
+            }
+        }
+
+        private static IEnumerable<FieldInfo> GetConstants(Type type)
+        {
+            return type.GetFields(BindingFlags.Public | BindingFlags.Static).Where(fi => fi.IsLiteral && !fi.IsInitOnly);
+        }
+    }
+}
